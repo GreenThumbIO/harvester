998b150a638ce7c6286d08f029228bb2618e2461
Kevin Welch <kaedub16@gmail.com>
2019-01-17 03:54:30 -0800

    Put database config data into .env file and added usage of dotenv library

--- app.py
+++ app.py
@@ -3,21 +3,20 @@ from flask import Flask
 from models import db, connect_db
 from flask_sqlalchemy import SQLAlchemy
 
+from dotenv import load_dotenv
+load_dotenv()
 
-DBUSER="postgres"
-DBPASS="greenthumb"
-DBNAME="postgres"
-DBPORT="5432"
-DBHOST="db"
-DBPORT="5432"
+# some default test values until we use dotenv
+DBUSER = os.getenv('POSTGRES_USER')
+DBPASS = os.getenv('POSTGRES_PASSWORD')
+DBNAME = os.getenv('POSTGRES_DB')
+DBPORT = os.getenv('POSTGRES_PORT')
+DBHOST = os.getenv('POSTGRES_HOST')
 
 def create_app():
     app = Flask(__name__)
 
-    # Get DB_URI from environ variable (useful for production/testing) or,
-    # if not set there, use development local db.
     # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or f'postgresql:///{DBNAME}'
-
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
         user=DBUSER,
         passwd=DBPASS,
@@ -28,11 +27,12 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = False
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
+    
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
     connect_db(app)
 
+    # test route
     @app.route('/')
     def hello_world():
         return 'Hello, World!'
