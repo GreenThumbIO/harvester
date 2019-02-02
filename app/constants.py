0ca901edf9f98fcd2a07011c0071e9e63400af77
Justin Bowen <JusBowen@gmail.com>
2019-02-02 00:09:47 -0800

    Adding constants

---
+++ app/constants.py
@@ -0,0 +1,13 @@
+"""
+app/constants.py - Somethings remain the same... like these constant variables
+"""
+
+# environments
+PRODUCTION = 'production'
+STAGING = 'staging'
+DEVELOPMENT = 'development'
+TEST = 'test'
+ENVIRONMENTS = (PRODUCTION, STAGING, DEVELOPMENT, TEST)
+
+# activities
+ACTIVITIES_TYPES = ('web', 'appliance')
